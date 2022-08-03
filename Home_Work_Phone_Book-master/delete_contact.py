2c154b0a736ee1e7eb43c4c7f5248e94fb88acae
Marina <troskom@mail.ru>
2022-08-03 21:26:44 +0300

    Correct_delete2

--- Home_Work_Phone_Book-master/delete_contact.py
+++ Home_Work_Phone_Book-master/delete_contact.py
@@ -1,21 +1,34 @@
 import json
 import controller
-
+from time import sleep
 path_to_db = 'db.json'
 
 def delete_contact():
-
     del_id = input('Введите id записи, которую необходимо удалить:  ')
     id = int(del_id)
+    try:
+        with open(path_to_db, 'r', encoding='UTF-8') as file:
+            data = json.load(file)
+            for i in range(0, id + 1):
+                if data[i]['id'] == id:
+                    del data[i]
+        with open(path_to_db, 'w', encoding='UTF-8') as file:
+            json.dump(data, file, indent=4)
+        print(f'\nЗапись с id {del_id} успешно удалёна!\n')
+        order_id()
+        sleep(2)
+        controller.user_choice()
+    except:
+        print(f'Записи с id {del_id} не существует\n')
+        sleep(2)
+        controller.user_choice()
 
+def order_id():
     with open(path_to_db, 'r', encoding='UTF-8') as file:
         data = json.load(file)
-        for i in range(0, id+1):
-            if data[i]['id'] == id:
-                del data[i]
-        # if name not in data:
-        #         print('Такого контакта нет')
+        for i in range(0, len(data)):
+             data[i]['id'] = i
     with open(path_to_db, 'w', encoding='UTF-8') as file:
         json.dump(data, file, indent=4)
-    print('\nКонтакт успешно изменена удалён!\n')
-    controller.user_choice()
+    print('id упорядочены!!\n')
+    sleep(1)
